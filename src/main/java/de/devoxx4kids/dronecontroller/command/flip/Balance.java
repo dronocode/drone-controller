4aac0cfe14a62d451b28f467069dce02c6cba3ff
Tobias Schneider <tobias.schneider@tobsch.org>
2016-03-10 19:57:20 +0100

    add new flip commands
    * Balance
    * DownsideDown
    * DownsideUp

---
+++ src/main/java/de/devoxx4kids/dronecontroller/command/flip/Balance.java
@@ -0,0 +1,51 @@
+package de.devoxx4kids.dronecontroller.command.flip;
+
+import de.devoxx4kids.dronecontroller.command.Acknowledge;
+import de.devoxx4kids.dronecontroller.command.ChannelType;
+import de.devoxx4kids.dronecontroller.command.Command;
+import de.devoxx4kids.dronecontroller.command.CommandKey;
+import de.devoxx4kids.dronecontroller.command.FrameType;
+
+
+/**
+ * @author  Tobias Schneider
+ */
+public final class Balance implements Command {
+
+    private final CommandKey commandKey = CommandKey.commandKey(3, 0, 1);
+
+    protected Balance() {
+
+        // use fabric method
+    }
+
+    public static Balance balance() {
+
+        return new Balance();
+    }
+
+
+    @Override
+    public byte[] getBytes(int counter) {
+
+        return new byte[] {
+                (byte) FrameType.ARNETWORKAL_FRAME_TYPE_DATA_WITH_ACK.ordinal(),
+                ChannelType.JUMPINGSUMO_CONTROLLER_TO_DEVICE_ACK_ID.getId(), (byte) counter, 15, 0, 0, 0,
+                commandKey.getProjectId(), commandKey.getClazzId(), commandKey.getCommandId(), 0, 0, 0, 0, 0
+            };
+    }
+
+
+    @Override
+    public Acknowledge getAcknowledge() {
+
+        return Acknowledge.AckBefore;
+    }
+
+
+    @Override
+    public String toString() {
+
+        return "Balance";
+    }
+}
