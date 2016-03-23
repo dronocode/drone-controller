673e848f95d71fb4dab500bc606b8f664936b759
Tobias Schneider <tobias.schneider@tobsch.org>
2016-03-23 19:06:35 +0100

    Improve Javadoc
    * PacketType

--- src/main/java/de/devoxx4kids/dronecontroller/command/PacketType.java
+++ src/main/java/de/devoxx4kids/dronecontroller/command/PacketType.java
@@ -7,26 +7,40 @@ package de.devoxx4kids.dronecontroller.command;
  *
  * <ul>
  *   <li>0 - UNINITIALIZED (Unknown type. Don't use)</li>
- *   <li>1 - ACK (Acknowledgment type)
+ *   <li>1 - ACK
  *
  *     <ul>
- *       <li>is used to acknowledge the IOControl command sequence number. Done by the device and by the controller. If
- *         the ACK-packet is not send within 100ms the device will send it again up to 3 times</li>
+ *       <li>Acknowledgment type</li>
+ *       <li>Incoming and Outgoing packet</li>
+ *       <li>is used to acknowledge the packet from type DATA_WITH_ACK with the sequence number. Done by the device and
+ *         by the controller. If the ACK-packet is not send within 100ms the device will send it again up to 3
+ *         times</li>
  *     </ul>
  *   </li>
- *   <li>2 - DATA (Data type. Main type for data that does not require an acknowledge)</li>
- *   <li>3 - DATA_LOW_LATENCY (Low latency data type. Should only be used when needed)
+ *   <li>2 - DATA
  *
  *     <ul>
- *       <li>Incoming-Only packet, carries the latest camera image starting from byte at position 13. The image is in
- *         jpeg format with a vga resolution (640x480px) with 15 frames per second</li>
+ *       <li>Data type</li>
+ *       <li>Incoming and Outgoing packet</li>
+ *       <li>Main type for data that does not require an acknowledge</li>
  *     </ul>
  *   </li>
- *   <li>4 - DATA_WITH_ACK (Data that should have an acknowledge type. This type can have a long latency)
+ *   <li>3 - DATA_LOW_LATENCY
  *
  *     <ul>
- *       <li>Bidirectional packet - over this packet type it is possible to send commands to the drone based on
- *         {@link Command}. The drone sends basic information over this packet type to communicate with the client
+ *       <li>Low latency data type. Should only be used when needed</li>
+ *       <li>Incoming-Only packet</li>
+ *       <li>Carries the latest camera image starting from byte at position 13. The image is in jpeg format with a vga
+ *         resolution (640x480px) with 15 frames per second</li>
+ *     </ul>
+ *   </li>
+ *   <li>4 - DATA_WITH_ACK
+ *
+ *     <ul>
+ *       <li>Data that should have an acknowledge type. This type can have a long latency</li>
+ *       <li>Incoming and Outgoing packet</li>
+ *       <li>used to send commands to the drone ({@link Command}). The drone sends basic information over this packet
+ *         type to communicate with the client
  *
  *         <ul>
  *           <li>Enabled Video streaming</li>
